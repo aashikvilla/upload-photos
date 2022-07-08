b25917762903a353c81d9d521cf76945e6795064
Aashik Villa <aashikvilla99@gmail.com>
2022-07-08 11:03:08 +0530

    save changes

---
+++ src/components/Home/Preview/ListItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ListItem() {
+  return (
+    <>
+    <div>
+        <image src=""/>
+        <button >delete</button>
+
+    </div>
+    </>
+  )
+}
+
+export default ListItem
